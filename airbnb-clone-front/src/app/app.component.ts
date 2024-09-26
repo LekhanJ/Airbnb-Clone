cc5d65c8ebd58c986b0762ac4450a8f3cfe2a2a9
LekhanJ <jomiwalelekhan@gmail.com>
2024-09-26 22:07:41 +0530

    Styles added

---
+++ airbnb-clone-front/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+}
